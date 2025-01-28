e00a2e6a97aa970c4bc39775b4a163b232f52cf5
Patrik Reizinger <prmedia17@gmail.com>
2025-01-28 16:53:36 +0100

    plot typography

--- plot_utils.py
+++ plot_utils.py
@@ -5,6 +5,40 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+from matplotlib import rc
+
+
+def plot_typography(
+    usetex: bool = False, small: int = 16, medium: int = 20, big: int = 22
+):
+    """
+    Initializes font settings and visualization backend (LaTeX or standard matplotlib).
+    :param usetex: flag to indicate the usage of LaTeX (needs LaTeX indstalled)
+    :param small: small font size in pt (for legends and axes' ticks)
+    :param medium: medium font size in pt (for axes' labels)
+    :param big: big font size in pt (for titles)
+    :return:
+    """
+
+    # font family
+    rc("font", **{"family": "sans-serif", "sans-serif": ["Helvetica"]})
+    ## for Palatino and other serif fonts use:
+    # rc('font',**{'family':'serif','serif':['Palatino']})
+
+    # backend
+    rc("text", usetex=usetex)
+    rc("font", family="serif")
+
+    # font sizes
+    rc("font", size=small)  # controls default text sizes
+    rc("axes", titlesize=big)  # fontsize of the axes title
+    rc("axes", labelsize=medium)  # fontsize of the x and y labels
+    rc("xtick", labelsize=small)  # fontsize of the tick labels
+    rc("ytick", labelsize=small)  # fontsize of the tick labels
+    rc("legend", fontsize=small)  # legend fontsize
+    rc("figure", titlesize=big)  # fontsize of the figure title
+
+
 def plot_estimates(estimate_list, true_tau, treatment_effect, title="Histogram of estimates"):
     # the histogram of the data
     n, bins, patches = plt.hist(estimate_list, 40, facecolor='green', alpha=0.75)
