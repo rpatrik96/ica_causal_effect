0b1fe2a541f9a4184b21c015ca41bf1be92731c9
Patrik Reizinger <prmedia17@gmail.com>
2025-09-29 12:29:53 +0200

    plots -> plot_utils.py

--- monte_carlo_single_instance.py
+++ monte_carlo_single_instance.py
@@ -16,7 +16,7 @@ from tueplots import bundles
 
 
 def experiment(x, eta, epsilon, treatment_effect, treatment_support, treatment_coef, outcome_support, outcome_coef,
-               eta_second_moment, eta_third_moment, lambda_reg, check_convergence=False, verbose=False):
+               eta_second_moment, eta_third_moment, lambda_reg, check_convergence=False, verbose=True):
     # Generate price as a function of co-variates
     treatment = np.dot(x[:, treatment_support], treatment_coef) + eta
     # Generate demand as a function of price and co-variates
@@ -289,11 +289,10 @@ def main(args):
                     plot_error_bar_stats(all_results, n_dim, n_experiments, n_samples, opts, beta)
 
     import seaborn as sns
-    import matplotlib.pyplot as plt
 
     def plot_heatmap(data_matrix, x_labels, y_labels, xlabel, ylabel, filename, output_dir, cmap="coolwarm",
                      center=None):
-        plot_typography()
+        plot_typography(small = 20, medium = 24, big = 30)
         plt.figure(figsize=(10, 8))
         # Set the midpoint of the color scale to the specified center if provided
         sns.heatmap(data_matrix, xticklabels=x_labels, yticklabels=y_labels, cmap=cmap, annot=True, center=center)
@@ -437,239 +436,20 @@ def main(args):
         plt.grid(True)
         plt.savefig(os.path.join(opts.output_dir, 'mse_vs_support_size.svg'))
 
+    plot_homl_ica_comparison_gennorm_beta_filter(all_results, opts, plot_heatmap, prepare_heatmap_data)
+    plot_homl_ica_comparison_gennorm_support_filter(all_results, opts, plot_heatmap, prepare_heatmap_data)
+    plot_oml_ica_comparison_gennorm_beta_filter(all_results, opts, plot_heatmap, prepare_heatmap_data)
+    plot_oml_ica_comparison_gennorm_support_filter(all_results, opts, plot_heatmap, prepare_heatmap_data)
 
-
-    # Plot heatmaps for comparison with HOML Split, filtered for beta=1
-    if opts.asymptotic_var is False:
-        bias_diff_matrix_dim_homl_mean, bias_diff_matrix_dim_homl_std, bias_diff_matrix_dim_homl, support_sizes, sample_sizes = prepare_heatmap_data(
-            all_results, 'support_size', 'n_samples', 'biases', diff_index=3, beta_filter=1)
-        plot_heatmap(bias_diff_matrix_dim_homl_mean, support_sizes, sample_sizes, r'$\dim X$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_dim_homl_mean.svg', opts.output_dir, center=0)
-        plot_heatmap(bias_diff_matrix_dim_homl_std, support_sizes, sample_sizes, r'$\dim X$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_dim_homl_std.svg', opts.output_dir, center=0)
-        plot_heatmap(bias_diff_matrix_dim_homl, support_sizes, sample_sizes, r'$\dim X$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_dim_homl.svg', opts.output_dir, center=0)
-
-    if opts.covariate_pdf == "gennorm" and opts.asymptotic_var is False:
-        bias_diff_matrix_beta_homl_mean, bias_diff_matrix_beta_homl_std, bias_diff_matrix_beta_homl, betas, sample_sizes = prepare_heatmap_data(
-            all_results, 'beta', 'n_samples', 'biases', diff_index=3, support_size_filter=10)
-        plot_heatmap(bias_diff_matrix_beta_homl_mean, betas, sample_sizes, r'$\beta$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_beta_homl_mean.svg', opts.output_dir, center=0)
-        plot_heatmap(bias_diff_matrix_beta_homl_std, betas, sample_sizes, r'$\beta$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_beta_homl_std.svg', opts.output_dir, center=0)
-        plot_heatmap(bias_diff_matrix_beta_homl, betas, sample_sizes, r'$\beta$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_beta_homl.svg', opts.output_dir, center=0)
-
-    # Plot heatmaps for comparison with OML, filtered for beta=1
-    if opts.asymptotic_var is False:
-        bias_diff_matrix_dim_oml_mean, bias_diff_matrix_dim_oml_std, bias_diff_matrix_dim_oml, support_sizes, sample_sizes = prepare_heatmap_data(
-            all_results, 'support_size', 'n_samples', 'biases', diff_index=0, beta_filter=1)
-        plot_heatmap(bias_diff_matrix_dim_oml_mean, support_sizes, sample_sizes, r'$\dim X$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_dim_oml_mean.svg', opts.output_dir, center=0)
-        plot_heatmap(bias_diff_matrix_dim_oml_std, support_sizes, sample_sizes, r'$\dim X$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_dim_oml_std.svg', opts.output_dir, center=0)
-        plot_heatmap(bias_diff_matrix_dim_oml, support_sizes, sample_sizes, r'$\dim X$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_dim_oml.svg', opts.output_dir, center=0)
-
-    if opts.covariate_pdf == "gennorm" and opts.asymptotic_var is False:
-        bias_diff_matrix_beta_oml_mean, bias_diff_matrix_beta_oml_std, bias_diff_matrix_beta_oml, betas, sample_sizes = prepare_heatmap_data(
-            all_results, 'beta', 'n_samples', 'biases', diff_index=0, support_size_filter=10)
-        plot_heatmap(bias_diff_matrix_beta_oml_mean, betas, sample_sizes, r'$\beta$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_beta_oml_mean.svg', opts.output_dir, center=0)
-        plot_heatmap(bias_diff_matrix_beta_oml_std, betas, sample_sizes, r'$\beta$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_beta_oml_std.svg', opts.output_dir, center=0)
-        plot_heatmap(bias_diff_matrix_beta_oml, betas, sample_sizes, r'$\beta$', r'$n$',
-                     'bias_diff_heatmap_sample_size_vs_beta_oml.svg', opts.output_dir, center=0)
-
-    # ICA error only
-    if opts.asymptotic_var is False:
-        ica_bias_matrix_dim_mean, ica_bias_matrix_dim_std, ica_bias_matrix_dim, support_sizes, sample_sizes = prepare_heatmap_data(
-            all_results, 'support_size', 'n_samples', 'biases', beta_filter=1)
-        plot_heatmap(ica_bias_matrix_dim_mean, support_sizes, sample_sizes, r'$\dim X$', r'$n$',
-                     'ica_bias_heatmap_sample_size_vs_dim_mean.svg', opts.output_dir, center=None)
-        plot_heatmap(ica_bias_matrix_dim_std, support_sizes, sample_sizes, r'$\dim X$', r'$n$',
-                     'ica_bias_heatmap_sample_size_vs_dim_std.svg', opts.output_dir, center=None)
-        # plot_heatmap(ica_bias_matrix_dim, support_sizes, sample_sizes, r'$\dim X$', r'$n$', 'ica_bias_heatmap_sample_size_vs_dim.svg', opts.output_dir, center=None)
-
-    if opts.asymptotic_var is False:
-        ica_mcc_matrix_dim_mean, ica_mcc_matrix_dim_std, ica_mcc_matrix_dim, support_sizes, sample_sizes = prepare_heatmap_data(
-            all_results, 'beta', 'n_samples', 'first_stage_mse', support_size_filter=10)
-        plot_heatmap(ica_mcc_matrix_dim_mean, support_sizes, sample_sizes, r'$\beta$', r'$n$',
-                     'ica_mcc_heatmap_sample_size_vs_beta_mean.svg', opts.output_dir, center=None)
-        plot_heatmap(ica_mcc_matrix_dim_std, support_sizes, sample_sizes, r'$\beta$', r'$n$',
-                     'ica_mcc_heatmap_sample_size_vs_beta_std.svg', opts.output_dir, center=None)
-        plot_heatmap(ica_mcc_matrix_dim, support_sizes, sample_sizes, r'$\beta$', r'$n$',
-                     'ica_mcc_heatmap_sample_size_vs_beta.svg', opts.output_dir, center=None)
-
-   
-    
-
-    if opts.covariate_pdf == "gennorm" and opts.asymptotic_var is False:
-        ica_bias_matrix_beta_mean, ica_bias_matrix_beta_std, ica_bias_matrix_beta, betas, sample_sizes = prepare_heatmap_data(
-            all_results, 'beta', 'n_samples', 'biases', support_size_filter=10)
-        plot_heatmap(ica_bias_matrix_beta_mean, betas, sample_sizes, r'$\beta$', r'$n$',
-                     'ica_bias_heatmap_sample_size_vs_beta_mean.svg', opts.output_dir, center=None)
-        plot_heatmap(ica_bias_matrix_beta_std, betas, sample_sizes, r'$\beta$', r'$n$',
-                     'ica_bias_heatmap_sample_size_vs_beta_std.svg', opts.output_dir, center=None)
-        # plot_heatmap(ica_bias_matrix_beta, betas, sample_sizes, r'$\beta$', r'$n$', 'ica_bias_heatmap_sample_size_vs_beta.svg', opts.output_dir, center=None)
+    plot_ica_gennorm_beta_filter(all_results, opts, plot_heatmap, prepare_heatmap_data)
+    plot_ica_gennorm_support_filter_mcc(all_results, opts, plot_heatmap, prepare_heatmap_data)
+    plot_ica_gennorm_beta_filter_bias(all_results, opts, plot_heatmap, prepare_heatmap_data)
 
 
 
     plot_asymptotic_var_comparison(all_results, opts)
 
     plot_multi_treatment(all_results, opts, treatment_effects)
-
-
-def plot_multi_treatment(all_results, opts, treatment_effects):
-    if len(treatment_effects) > 1:
-        # Prepare data for the scatter plot
-        x_values_true_theta = [res['treatment_effect'] for res in
-                               all_results]  # Assuming the true value of theta is the first element of true_coef_outcome
-        y_values_ica_asymptotic_var = [res['ica_asymptotic_var'] for res in all_results]
-        y_values_homl_asymptotic_var = [res['homl_asymptotic_var'] for res in all_results]
-        y_values_actual_variance_ica = [res['sigmas'][-1] ** 2 * res['n_samples'] for res in all_results]
-        y_values_actual_variance_homl = [res['sigmas'][3] ** 2 * res['n_samples'] for res in all_results]
-
-        # Scatter plot for true theta vs variances
-        plt.figure(figsize=(10, 8))
-        plt.scatter(x_values_true_theta, y_values_ica_asymptotic_var, c='blue', alpha=0.75,
-                    label='ICA Asymptotic Variance')
-        plt.scatter(x_values_true_theta, y_values_actual_variance_ica, c='green', alpha=0.75,
-                    label='ICA Actual Variance')
-        plt.scatter(x_values_true_theta, y_values_homl_asymptotic_var, c='red', alpha=0.75,
-                    label='HOML Asymptotic Variance')
-        plt.scatter(x_values_true_theta, y_values_actual_variance_homl, c='orange', alpha=0.75,
-                    label='HOML Actual Variance')
-        plt.xlabel('True Value of Theta')
-        plt.ylabel('Variance')
-        plt.legend()
-        plt.savefig(os.path.join(opts.output_dir, 'scatter_plot_true_theta_vs_variances.svg'), dpi=300,
-                    bbox_inches='tight')
-        plt.close()
-
-        # Prepare data for the scatter plot of MSEs against theta
-        x_values_true_theta = [res['treatment_effect'] for res in all_results]
-        y_values_ica_mse = [res['biases'][-1] for res in all_results]
-        y_values_homl_mse = [res['biases'][3] for res in all_results]
-        y_errors_ica = [res['sigmas'][-1] for res in all_results]
-        y_errors_homl = [res['sigmas'][3] for res in all_results]
-
-        # Bar plot for true theta vs MSEs with variances
-        plt.figure(figsize=(10, 8))
-        bar_width = 0.35
-        index = np.arange(len(x_values_true_theta))
-
-        plt.bar(index, y_values_ica_mse, bar_width, color='blue', alpha=0.75, label='ICA MSE', yerr=y_errors_ica,
-                capsize=5)
-        plt.bar(index + bar_width, y_values_homl_mse, bar_width, color='red', alpha=0.75, label='HOML MSE',
-                yerr=y_errors_homl, capsize=5)
-
-        plt.xticks(ticks=index + bar_width / 2, labels=[f"{theta:.2f}" for theta in x_values_true_theta], rotation=45)
-        plt.xlabel('ICA var coeff')
-        plt.ylabel('Mean Squared Error (MSE)')
-        plt.legend()
-        plt.tight_layout()
-        plt.savefig(os.path.join(opts.output_dir, 'bar_plot_true_theta_vs_mses_with_variances.svg'), dpi=300,
-                    bbox_inches='tight')
-        plt.close()
-
-
-def plot_asymptotic_var_comparison(all_results, opts):
-    if opts.asymptotic_var:
-        return 0
-
-
-    # Prepare data for the new scatter plot
-    x_values_var_diff = [res['ica_asymptotic_var'] - res['homl_asymptotic_var'] for res in all_results]
-    x_values_var__hyvarinen_diff = [res['ica_asymptotic_var_hyvarinen'] - res['homl_asymptotic_var'] for res in
-                                    all_results]
-    y_values_bias_diff = [res['biases'][-1] - res['biases'][3] for res in all_results]
-    y_values_sigma_diff = [res['sigmas'][-1] - res['sigmas'][3] for res in all_results]
-    colors = [res['beta'] for res in all_results]
-    plt.figure(figsize=(10, 8))
-    scatter = plt.scatter(x_values_var_diff, y_values_bias_diff, c=colors, cmap='viridis', alpha=0.75)
-    plt.colorbar(scatter, label='Beta')
-    plt.xlabel('Difference in Asymptotic Variance (ICA - HOML)')
-    plt.ylabel('Difference in Bias (ICA - HOML)')
-    # plt.title('Scatter Plot: Asymptotic Variance vs Bias Difference')
-    plt.savefig(os.path.join(opts.output_dir, 'scatter_plot_var_vs_bias_diff.svg'), dpi=300, bbox_inches='tight')
-    plt.close()
-    plt.figure(figsize=(10, 8))
-    scatter = plt.scatter(x_values_var_diff, y_values_sigma_diff, c=colors, cmap='viridis', alpha=0.75)
-    plt.colorbar(scatter, label='Beta')
-    plt.xlabel('Difference in Asymptotic Variance (ICA - HOML)')
-    plt.ylabel('Difference in Variance (ICA - HOML)')
-    # plt.title('Scatter Plot: Asymptotic Variance vs Bias Difference')
-    plt.savefig(os.path.join(opts.output_dir, 'scatter_plot_var_vs_asy_var_diff.svg'), dpi=300, bbox_inches='tight')
-    plt.close()
-    plt.figure(figsize=(10, 8))
-    scatter = plt.scatter(x_values_var__hyvarinen_diff, y_values_sigma_diff, c=colors, cmap='viridis', alpha=0.75)
-    plt.colorbar(scatter, label='Beta')
-    plt.xlabel('Difference in Asymptotic Variance (ICA - HOML) Hyvarinen')
-    plt.ylabel('Difference in Variance (ICA - HOML)')
-    # plt.title('Scatter Plot: Asymptotic Variance vs Bias Difference')
-    plt.savefig(os.path.join(opts.output_dir, 'scatter_plot_var_vs_asy_var_diff_hyvarinen.svg'), dpi=300,
-                bbox_inches='tight')
-    plt.close()
-    # Prepare data for the scatter plots
-    x_values_sample_size = [res['n_samples'] for res in all_results]
-    y_values_ica_asymptotic_var = [res['ica_asymptotic_var'] for res in all_results]
-    y_values_ica_asymptotic_var_hyvarinen = [res['ica_asymptotic_var_hyvarinen'] for res in all_results]
-    y_values_homl_asymptotic_var = [res['homl_asymptotic_var'] for res in all_results]
-    y_values_actual_variance_ica = [res['sigmas'][-1] ** 2 * res['n_samples'] for res in all_results]
-    y_values_actual_variance_homl = [res['sigmas'][3] ** 2 * res['n_samples'] for res in all_results]
-    # colors = [res['beta'] for res in all_results]
-    # Scatter plot for ICA asymptotic and actual variance
-    plt.figure(figsize=(10, 8))
-    plt.scatter(x_values_sample_size, y_values_ica_asymptotic_var, c='blue', alpha=0.75,
-                label='ICA Asymptotic Variance')
-    plt.scatter(x_values_sample_size, y_values_actual_variance_ica, c='red', alpha=0.75, label='ICA Actual Variance')
-    plt.xlabel('Sample Size')
-    plt.ylabel('Variance')
-    plt.legend()
-    plt.savefig(os.path.join(opts.output_dir, 'scatter_plot_sample_size_vs_ica_variance.svg'), dpi=300,
-                bbox_inches='tight')
-    plt.close()
-    # Scatter plot for ICA Hyvarinen asymptotic and actual variance
-    plt.figure(figsize=(10, 8))
-    plt.scatter(x_values_sample_size, y_values_ica_asymptotic_var_hyvarinen, c='blue', alpha=0.75,
-                label='ICA Hyvarinen Asymptotic Variance')
-    plt.scatter(x_values_sample_size, y_values_actual_variance_ica, c='red', alpha=0.75, label='ICA Actual Variance')
-    plt.xlabel('Sample Size')
-    plt.ylabel('Variance')
-    plt.legend()
-    plt.savefig(os.path.join(opts.output_dir, 'scatter_plot_sample_size_vs_ica_hyvarinen_variance.svg'), dpi=300,
-                bbox_inches='tight')
-    plt.close()
-    # Scatter plot for HOML asymptotic and actual variance
-    plt.figure(figsize=(10, 8))
-    plt.scatter(x_values_sample_size, y_values_homl_asymptotic_var, c='blue', alpha=0.75,
-                label='HOML Asymptotic Variance')
-    plt.scatter(x_values_sample_size, y_values_actual_variance_homl, c='red', alpha=0.75, label='HOML Actual Variance')
-    plt.xlabel('Sample Size')
-    plt.ylabel('Variance')
-    plt.legend()
-    plt.savefig(os.path.join(opts.output_dir, 'scatter_plot_sample_size_vs_homl_variance.svg'), dpi=300,
-                bbox_inches='tight')
-    plt.close()
-    # Prepare data for the scatter plot
-    x_values_ica_var_coeff = [res['ica_var_coeff'] for res in all_results]
-    y_values_ica_biases = [res['biases'][-1] for res in all_results]
-    y_values_homl_biases = [res['biases'][3] for res in all_results]
-    y_errors_ica = [res['sigmas'][-1] / np.sqrt(res['n_samples']) for res in all_results]
-    y_errors_homl = [res['sigmas'][3] / np.sqrt(res['n_samples']) for res in all_results]
-    # Scatter plot for ICA and HOML biases with sigmas as error bars
-    plt.figure(figsize=(10, 8))
-    plt.errorbar(x_values_ica_var_coeff, y_values_ica_biases, yerr=y_errors_ica, fmt='o', color='blue', alpha=0.75,
-                 label='ICA')
-    plt.errorbar(x_values_ica_var_coeff, y_values_homl_biases, yerr=y_errors_homl, fmt='o', color='red', alpha=0.75,
-                 label='HOML')
-    plt.xlabel(r'$1+(b+a\theta)^2$')
-    plt.xscale('log')
-    plt.ylabel(r'$\mathrm{Var}(\hat{\theta})$')
-    plt.legend()
-    plt.savefig(os.path.join(opts.output_dir, 'scatter_plot_ica_var_coeff_vs_biases.svg'), dpi=300, bbox_inches='tight')
-    plt.close()
 
 
 def calc_ica_asymptotic_var(eta_cubed_variance, eta_fourth_moment, eta_third_moment, outcome_coef, sigma_outcome,
